e04d898d4d50c6a12357ea9709b6ad39cd8d029c
BustANut <59153118+BustANut@users.noreply.github.com>
2022-03-19 18:37:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from WeatherAPI import weather
 
 
-class BoxLayout1(BoxLayout):
+class BoxLayout1(AnchorLayout):
     pass
 
 
 class OraiApp(App):
-    city_name = StringProperty("Kretinga")
-    temp = weather(str("kretinga"))[1]
+    def build(self):
+        self.icon = "images/Weather_Icon.png"
+        pass
 
     def on_click(self):
         if self.root.ids.textinput.text == '':
+            pass
+        elif self.root.ids.textinput.text.title() == self.root.ids.my_label.text:
             pass
         else:
             textinput = self.root.ids.textinput
@@ -32,6 +34,7 @@ class OraiApp(App):
                 pressure.text = "Slėgis: " + str(str(weather(str(textinput.text))[2])) + " Pa"
                 humidity.text = "Drėgmė: " + str(str(weather(str(textinput.text))[3])) + "%"
                 feels_like.text = "Jaučiasi kaip: " + str(str(weather(str(textinput.text))[4])) + "°"
+                textinput.text = ""
 
 
 OraiApp().run()
